408d7f25fff76d584b7eec88474da67f99dc7560
YoonHan KIM <reprica123@gmail.com>
2023-04-07 17:51:04 +0900

    복습

---
+++ Main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	// 1부터 100까지 짝수의 합 (for문)
+
+	//int a = 0;
+
+	//for (int i = 1; i < 101; i++)
+	//{
+	//	if (i % 2 == 0)
+	//	{
+	//		a += i;
+	//	}
+	//}
+
+	//cout << a << endl;
+
+	//for (int i = 0; i < 6; i++)
+	//{
+	//	for (int j = 0; j < i; j++)
+	//	{
+	//		cout << '*';
+	//	}
+	//	cout << '\n';
+	//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
